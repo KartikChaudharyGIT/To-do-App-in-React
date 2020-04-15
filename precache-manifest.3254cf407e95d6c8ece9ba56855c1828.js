994a96f66a20007d6ec340142f033ed1aed44fef
kartikchaudharyGIT <kartikchaudhary89@gmail.com>
2020-04-15 14:27:28 +0530

    Updates

---
+++ precache-manifest.3254cf407e95d6c8ece9ba56855c1828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24eaaae19f6063af292f1a2c9888ca9",
+    "url": "/To-do-app-in-React/index.html"
+  },
+  {
+    "revision": "5e11f837d1ba74166625",
+    "url": "/To-do-app-in-React/static/css/main.96c3b527.chunk.css"
+  },
+  {
+    "revision": "105ed4a34ac76854b691",
+    "url": "/To-do-app-in-React/static/js/2.a76757de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app-in-React/static/js/2.a76757de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e11f837d1ba74166625",
+    "url": "/To-do-app-in-React/static/js/main.1d985974.chunk.js"
+  },
+  {
+    "revision": "9c1bc862acec03db6089",
+    "url": "/To-do-app-in-React/static/js/runtime-main.0370adc0.js"
+  }
+]);
